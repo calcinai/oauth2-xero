4a4ed2586698260e9ae652df45d4a0eebf40591e
Jason Judge <jason.judge@academe.co.uk>
2020-09-06 13:35:20 +0100

    Issue #6 additional properties for resource owner
    Some additional useful properties and also the ability to access them as camelCase (without changing the underlying property names to avoid a BC break)

--- src/XeroResourceOwner.php
+++ src/XeroResourceOwner.php
@@ -38,12 +38,38 @@ class XeroResourceOwner
     public $family_name;
 
     /**
+     * @var string the unique user ID, different to Xero user ID
+     */
+    public $sub;
+
+    /**
+     * @var int details expirey time
+     */
+    public $exp;
+
+    /**
+     * @var int the time the authorisation was first granted for this token
+     */
+    public $auth_time;
+
+    /**
+     * @var int token issued at time; last authentication flow time
+     */
+    public $iat;
+
+    /**
+     * @var string
+     */
+    public $aud;
+
+    /**
      * @param $token
      * @return static
      */
     public static function fromJWT($token)
     {
         list($header, $body, $crypto) = explode('.', $token);
+
         //This needs to be done manually as we don't get a signed JWT
         $decoded = JWT::jsonDecode(JWT::urlsafeB64Decode($body));
 
@@ -54,7 +80,27 @@ class XeroResourceOwner
         $self->email = $decoded->email;
         $self->given_name = $decoded->given_name;
         $self->family_name = $decoded->family_name;
+        $self->sub = $decoded->sub;
+        $self->exp = $decoded->exp;
+        $self->auth_time = $decoded->auth_time;
+        $self->iat = $decoded->iat;
+        $self->aud = $decoded->aud;
 
         return $self;
+    }
+
+    /**
+     * Provide camelCase access to the underlying properties.
+     *
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $snakeCase = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $name));
+
+        if (property_exists($this, $snakeCase)) {
+            return $this->$snakeCase;
+        }
     }
 }
